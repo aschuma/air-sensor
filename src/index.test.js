36224667e3f2a3ec7de4a37a55121951e3636cce
mail <aschuma@users.noreply.github.com>
2018-02-03 22:54:21 +0100

    chore: Travis CI

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+const expect = require("chai").expect;
+const sensor = require("./index.js");
+
+describe("air-sensor", function () {
+    it("should work", function () {
+        expect(true).to.be.true;
+    });
+});
